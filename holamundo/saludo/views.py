01f9b5c82b78c47694eb2e437b7ccf35404d7038
Collective Cloud Peru <collectivecloudperu@users.noreply.github.com>
2017-04-20 02:00:57 -0500

    Update views.py

--- holamundo/saludo/views.py
+++ holamundo/saludo/views.py
@@ -8,7 +8,7 @@ def saludo(request):
 	blog = "http://blog.collectivecloudperu.com"
 	titulo = "Entorno de Trabajo con Bootstrap, Django y React JS"
 	titulo_video_youtube ="5G y Samsung cambiarán la tecnología móvil."
-	video_youtube = "https://www.youtube.com/v/Bx3Xr7IeUmw"
+	video_youtube = "https://www.youtube.com/embed/Bx3Xr7IeUmw"
 	context = {    	
 		'autor': autor,
 		'web': web,
